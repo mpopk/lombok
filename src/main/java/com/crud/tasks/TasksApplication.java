2dfa3e2af6bc715f6a12c6f6141f9b5c84dcfbcf
mpopk <m.popkiewicz@gmail.com>
2018-04-08 19:19:42 +0200

    Task_Controller

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -16,7 +16,7 @@ public class TasksApplication {
 
         System.out.println(id + " " + title + " " + content);
 
-        //SpringApplication.run(TasksApplication.class, args);
+        SpringApplication.run(TasksApplication.class, args);
 
     }
 }
